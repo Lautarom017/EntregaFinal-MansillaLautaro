9cb2b2b61006356560eda0dc5940e5a044e09db9
LautaroM <mansillal.1997@gmail.com>
2024-04-22 19:31:06 -0300

    volvimos a empezar, porque la peticion fetch que habiamos creado estaba mal

--- js/script.js
+++ js/script.js
@@ -14,15 +14,15 @@ let highScore = localStorage.getItem("high-score") || 0;
 highScoreElement.innerHTML = `High Score: ${highScore}`;
 
 const changeFoodPosition = () => {
-    foodX = Math.floor(Math.random() * 30) +1;
-    foodY = Math.floor(Math.random() * 30) +1;
-}
+    foodX = Math.floor(Math.random() * 30) + 1;
+    foodY = Math.floor(Math.random() * 30) + 1;
+};
 
 const handleGameOver = () => {
     clearInterval(setIntervalId);
     Swal.fire({
         title: 'Game Over!',
-        text: 'Has perdido! Vuelve a intentarlo..',
+        text: 'Has perdido ! Vuelve a intentarlo..',
         icon: 'error',
         showCancelButton: false,
         confirmButtonColor: '#3085d6',
@@ -35,74 +35,59 @@ const handleGameOver = () => {
 };
 
 const changeDirection = (e) => {
-    if(e.key === "ArrowUp" && velocityY != 1) {
+    if (e.key === "ArrowUp" && velocityY != 1) {
         velocityX = 0;
         velocityY = -1;
-    } else if(e.key === "ArrowDown" && velocityY != -1) {
+    } else if (e.key === "ArrowDown" && velocityY != -1) {
         velocityX = 0;
         velocityY = 1;
-    } else if(e.key === "ArrowLeft" && velocityX != 1) {
+    } else if (e.key === "ArrowLeft" && velocityX != 1) {
         velocityX = -1;
         velocityY = 0;
-    } else if(e.key === "ArrowRight" && velocityX != -1) {
+    } else if (e.key === "ArrowRight" && velocityX != -1) {
         velocityX = 1;
         velocityY = 0;
     }
-}
+};
 
 const initGame = () => {
-    if(gameOver) return handleGameOver();
+    if (gameOver) return handleGameOver();
     let htmlMarkup = `<div class="food" style="grid-area: ${foodY} / ${foodX}"></div>`;
-    
-    if(snakeX === foodX && snakeY === foodY) {
+
+    if (snakeX === foodX && snakeY === foodY) {
         changeFoodPosition();
         snakeBody.push([foodX, foodY]);
         score++;
-        
+
         highScore = score >= highScore ? score : highScore;
         localStorage.setItem("high-score", highScore);
         scoreElement.innerHTML = `Score: ${score}`;
-        
+
         highScoreElement.innerHTML = `High Score: ${highScore}`;
     }
 
-    for(let i = snakeBody.length - 1; i > 0; i--){
+    for (let i = snakeBody.length - 1; i > 0; i--) {
         snakeBody[i] = snakeBody[i - 1];
     }
-    
-    snakeBody[0] = [snakeX, snakeY]
-    
+
+    snakeBody[0] = [snakeX, snakeY];
+
     snakeX += velocityX;
     snakeY += velocityY;
-    
-    if(snakeX <= 0 || snakeX > 30 || snakeY <= 0 || snakeY > 30) {
+
+    if (snakeX <= 0 || snakeX > 30 || snakeY <= 0 || snakeY > 30) {
         gameOver = true;
     }
 
-    for(let i = 0; i < snakeBody.length; i++) {
-        htmlMarkup += `<div class="head" style="grid-area: ${snakeBody[i][1]} / ${snakeBody[i][0]}; background-color: ${randomColor()}"></div>`;
-        if(i !== 0 && snakeBody[0][1] === snakeBody[i][1] && snakeBody[0][0] === snakeBody[i][0]) {
+    for (let i = 0; i < snakeBody.length; i++) {
+        htmlMarkup += `<div class="head" style="grid-area: ${snakeBody[i][1]} / ${snakeBody[i][0]}"></div>`;
+        if (i !== 0 && snakeBody[0][1] === snakeBody[i][1] && snakeBody[0][0] === snakeBody[i][0]) {
             gameOver = true;
         }
     }
     playBoard.innerHTML = htmlMarkup;
-}
+};
 
-const randomColor = async () => {
-    try {
-        const response = await fetch('https://reqres.in/api/products');
-        if (!response.ok) {
-            throw new Error('Error al obtener los colores');
-        }
-        const data = await response.json();
-        const colors = ['#FF5733', '#33FF57', '#5733FF', '#33FFF9', '#FF33F9', '#F933FF', '#33FFF0', '#F0FF33', '#FFD333', '#FF333F'];
-        return colors[Math.floor(Math.random() * colors.length)];
-    } catch (error) {
-        console.error('Error al obtener los colores:', error.message);
-        return '#000000';
-    }
-}
-
-changeFoodPosition();
-setIntervalId = setInterval(initGame, 125)
 document.addEventListener("keydown", changeDirection);
+changeFoodPosition();
+setIntervalId = setInterval(initGame, 125);
